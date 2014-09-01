58d54e4043cac564664a4c391b2dffcc84e636d1
kuzko <pj2000@hotmail.fr>
2014-09-01 12:00:38 +0200

    ffmpeg doesn't install system wide

--- seedbox-from-scratch.sh
+++ seedbox-from-scratch.sh
@@ -370,7 +370,8 @@ apt-get -y install autoconf automake build-essential libass-dev libfreetype6-dev
 wget http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz
 tar xzvf yasm-1.3.0.tar.gz
 cd yasm-1.3.0
-./configure --prefix="/usr/local/src/ffmpeg_build" --bindir="/usr/local/bin"
+./configure 
+#--prefix="/usr/local/src/ffmpeg_build" --bindir="/usr/local/bin"
 make
 make install
 make distclean
@@ -378,7 +379,8 @@ cd .. && rm -rf yasm-1.3.0 yasm-1.3.0.tar.gz
 #libx264
 git clone --depth 1 git://git.videolan.org/x264.git
 cd x264
-./configure --enable-static --prefix="/usr/local/src/ffmpeg_build" --bindir="/usr/local/bin"
+./configure --enable-static 
+#--prefix="/usr/local/src/ffmpeg_build" --bindir="/usr/local/bin"
 make
 make install
 make distclean
@@ -387,7 +389,8 @@ cd ..
 git clone --depth 1 git://github.com/mstorsjo/fdk-aac.git
 cd fdk-aac
 autoreconf -fiv
-./configure --disable-shared --prefix="/usr/local/src/ffmpeg_build" --bindir="/usr/local/bin"
+./configure --disable-shared 
+#--prefix="/usr/local/src/ffmpeg_build" --bindir="/usr/local/bin"
 make
 make install
 make distclean
@@ -398,7 +401,8 @@ apt-get install --force-yes -y libmp3lame-dev
 wget http://downloads.xiph.org/releases/opus/opus-1.1.tar.gz
 tar xzvf opus-1.1.tar.gz
 cd opus-1.1
-./configure --disable-shared --prefix="/usr/local/src/ffmpeg_build" --bindir="/usr/local/bin"
+./configure --disable-shared 
+#--prefix="/usr/local/src/ffmpeg_build" --bindir="/usr/local/bin"
 make
 make install
 make distclean
@@ -407,7 +411,8 @@ cd ..
 wget http://webm.googlecode.com/files/libvpx-v1.3.0.tar.bz2
 tar xjvf libvpx-v1.3.0.tar.bz2
 cd libvpx-v1.3.0
-./configure --disable-examples --prefix="/usr/local/src/ffmpeg_build"
+./configure --disable-examples 
+#--prefix="/usr/local/src/ffmpeg_build"
 make
 make install
 make clean
@@ -416,7 +421,8 @@ cd ..
 wget http://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
 tar xjvf ffmpeg-snapshot.tar.bz2
 cd ffmpeg
-./configure --prefix="/usr/local/src/ffmpeg_build" --extra-cflags="-I/usr/local/src/ffmpeg_build/include" --extra-ldflags="-L/usr/local/src/ffmpeg_build/lib" --bindir="/usr/local/bin" --extra-libs="-ldl" --enable-gpl --enable-libass --enable-libfaac --enable-libfdk-aac --enable-libmp3lame --enable-libopus --enable-postproc --enable-libtheora --enable-libvorbis --enable-libvpx --enable-libx264 --enable-nonfree
+./configure --extra-libs="-ldl" --enable-gpl --enable-libass --enable-libfaac --enable-libfdk-aac --enable-libmp3lame --enable-libopus --enable-postproc --enable-libtheora --enable-libvorbis --enable-libvpx --enable-libx264 --enable-nonfree
+#--prefix="/usr/local/src/ffmpeg_build" --extra-cflags="-I/usr/local/src/ffmpeg_build/include" --extra-ldflags="-L/usr/local/src/ffmpeg_build/lib" --bindir="/usr/local/bin"
 make
 make install
 make distclean
