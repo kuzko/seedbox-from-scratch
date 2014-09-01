f0516c61fb8381756112765e4287d404f2b4aa09
kuzko <pj2000@hotmail.fr>
2014-09-01 12:50:01 +0200

    well... forgetting the source install...
    forget previous commits... they're useless crap

--- seedbox-from-scratch.sh
+++ seedbox-from-scratch.sh
@@ -355,78 +355,13 @@ if [ $? -gt 0 ]; then
   exit 1
 fi
 
-#FFmpeg from source install 
+#FFmpeg from deb-multimedia install 
 echo "deb http://www.deb-multimedia.org $(lsb_release -cs | sed -n '/lucid\|precise\|quantal\|raring\|saucy\|trusty\|squeeze\|wheezy\|jessie\|sid/p') main non-free" >> /etc/apt/sources.list
 echo "deb-src http://www.deb-multimedia.org $(lsb_release -cs | sed -n '/lucid\|precise\|quantal\|raring\|saucy\|trusty\|squeeze\|wheezy\|jessie\|sid/p') main non-free" >> /etc/apt/sources.list
 apt-get update
 apt-get install --force-yes -y deb-multimedia-keyring
 apt-get update
-cd /usr/local/src/
-mkdir ffmpeg_sources
-cd ffmpeg_sources/
-#dependencies :
-apt-get -y install autoconf automake build-essential libass-dev libfreetype6-dev libgpac-dev libtheora-dev libtool libvorbis-dev pkg-config texi2html zlib1g-dev
-#YASM
-wget http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz
-tar xzvf yasm-1.3.0.tar.gz
-cd yasm-1.3.0
-./configure 
-#--prefix="/usr/local/src/ffmpeg_build" --bindir="/usr/local/bin"
-make
-make install
-make distclean
-cd .. && rm -rf yasm-1.3.0 yasm-1.3.0.tar.gz
-#libx264
-git clone --depth 1 git://git.videolan.org/x264.git
-cd x264
-./configure --enable-static 
-#--prefix="/usr/local/src/ffmpeg_build" --bindir="/usr/local/bin"
-make
-make install
-make distclean
-cd ..
-# libfdk-aac
-git clone --depth 1 git://github.com/mstorsjo/fdk-aac.git
-cd fdk-aac
-autoreconf -fiv
-./configure --disable-shared 
-#--prefix="/usr/local/src/ffmpeg_build" --bindir="/usr/local/bin"
-make
-make install
-make distclean
-cd ..
-#libmp3lame
-apt-get install --force-yes -y libmp3lame-dev
-#libopus
-wget http://downloads.xiph.org/releases/opus/opus-1.1.tar.gz
-tar xzvf opus-1.1.tar.gz
-cd opus-1.1
-./configure --disable-shared 
-#--prefix="/usr/local/src/ffmpeg_build" --bindir="/usr/local/bin"
-make
-make install
-make distclean
-cd ..
-#libvpx
-wget http://webm.googlecode.com/files/libvpx-v1.3.0.tar.bz2
-tar xjvf libvpx-v1.3.0.tar.bz2
-cd libvpx-v1.3.0
-./configure --disable-examples 
-#--prefix="/usr/local/src/ffmpeg_build"
-make
-make install
-make clean
-cd ..
-#final install
-wget http://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
-tar xjvf ffmpeg-snapshot.tar.bz2
-cd ffmpeg
-./configure --extra-libs="-ldl" --enable-gpl --enable-libass --enable-libfaac --enable-libfdk-aac --enable-libmp3lame --enable-libopus --enable-postproc --enable-libtheora --enable-libvorbis --enable-libvpx --enable-libx264 --enable-nonfree
-#--prefix="/usr/local/src/ffmpeg_build" --extra-cflags="-I/usr/local/src/ffmpeg_build/include" --extra-ldflags="-L/usr/local/src/ffmpeg_build/lib" --bindir="/usr/local/bin"
-make
-make install
-make distclean
-hash -r
+apt-get install --force-yes -y ffmpeg
 if [ $? -gt 0 ]; then
   set +x verbose
   echo
